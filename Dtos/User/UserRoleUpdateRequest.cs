eba658e36988fbe4703b121704c7fad5cc8a9232
Ahmed Halim <108583244+ahmedhalimx@users.noreply.github.com>
2026-08-17 22:47:24 +0300

    created User DTOs

---
+++ Dtos/User/UserRoleUpdateRequest.cs
@@ -0,0 +1,7 @@
+namespace FulfillmentInventoryPlatform.Dtos.User;
+
+
+public class UserRoleUpdateRequest
+{
+    public string Role { get; set; } = string.Empty;
+}
---
+++ Dtos/User/UserWarehouseAssignRequest.cs
@@ -0,0 +1,7 @@
+namespace FulfillmentInventoryPlatform.Dtos.User;
+
+
+public class UserWarehouseAssignRequest
+{
+    public List<int> WarehouseIds { get; set; } = new();
+}
