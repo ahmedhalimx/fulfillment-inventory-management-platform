5fa505e32fbf18ef041904902e887399fa73d4aa
Ahmed Halim <108583244+ahmedhalimx@users.noreply.github.com>
2026-08-17 22:32:51 +0300

    created StockItem model

---
+++ Models/StockItem.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FulfillmentInventoryPlatform.Models;
+
+
+public class StockItem
+{
+    public int Id { get; set; }
+
+    public int ProductId { get; set; }
+
+    public int WarehouseId { get; set; }
+
+    public int Quantity { get; set; }
+
+    public DateTime LastUpdatedAt { get; set; } = DateTime.UtcNow;
+
+
+    public Product Product { get; set; } = null!;
+
+    public Warehouse Warehouse { get; set; } = null!;
+
+    public ICollection<StockAdjustment> StockAdjustments { get; set; } = new List<StockAdjustment>();
+}
