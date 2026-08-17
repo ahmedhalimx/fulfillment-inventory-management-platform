bb60539edc074c106548c7b0054dd9093932c619
Ahmed Halim <108583244+ahmedhalimx@users.noreply.github.com>
2026-08-17 22:29:42 +0300

    created Product model

---
+++ Models/Product.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FulfillmentInventoryPlatform.Models;
+
+
+public class Product
+{
+    public int Id { get; set; }
+
+    [Required, MaxLength(200)]
+    public string Name { get; set; } = string.Empty;
+
+    [MaxLength(1000)]
+    public string Description { get; set; } = string.Empty;
+
+    [Required, MaxLength(50)]
+    public string SKU { get; set; } = string.Empty;
+
+    public int CategoryId { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+
+    public Category Category { get; set; } = null!;
+
+    public ICollection<StockItem> StockItems { get; set; } = new List<StockItem>();
+}
