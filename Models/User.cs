aecc18a2b134fae30f4cf8ce316bc42e5cdea6c1
Ahmed Halim <108583244+ahmedhalimx@users.noreply.github.com>
2026-08-17 22:26:29 +0300

    created User model

---
+++ Models/User.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FulfillmentInventoryPlatform.Models;
+
+
+public class User
+{
+    public int Id { get; set; }
+
+    [Required, MaxLength(100)]
+    public string Username { get; set; } = string.Empty;
+
+    [Required, MaxLength(200)]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    public string PasswordHash { get; set; } = string.Empty;
+
+    [Required, MaxLength(100)]
+    public string FullName { get; set; } = string.Empty;
+
+    [Required]
+    public string Role { get; set; } = "Operator";
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+
+    public ICollection<UserWarehouse> UserWarehouses { get; set; } = new List<UserWarehouse>();
+    public ICollection<StockAdjustment> StockAdjustments { get; set; } = new List<StockAdjustment>();
+}
